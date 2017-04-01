c50448fab7dc92f7b5359f6f976fe3f7ac2c2c0a
yeyi <yun_198432@126.com>
2017-04-01 17:28:27 +0800

    添加.gitignore文件

---
+++ app/src/main/java/com/aile/cloud/MyLeanCloudApp.java
@@ -0,0 +1,20 @@
+package com.aile.cloud;
+
+import android.app.Application;
+
+import com.avos.avoscloud.AVOSCloud;
+
+/**
+ * Created by yeyi on 17/4/1.
+ */
+
+public class MyLeanCloudApp extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        // 初始化参数依次为 this, AppId, AppKey
+        AVOSCloud.initialize(this,"9ThylQduJJxQhqOEpqwooFUN-gzGzoHsz","JPNipjTp6Qan66INHQ67BE7Q");
+    }
+}
