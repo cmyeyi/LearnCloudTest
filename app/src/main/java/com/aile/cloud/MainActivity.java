3b4f9d3bc0020a8528890ef44823a4508e13274e
yeyi <yun_198432@126.com>
2017-04-01 18:01:42 +0800

    test request

--- app/src/main/java/com/aile/cloud/MainActivity.java
+++ app/src/main/java/com/aile/cloud/MainActivity.java
@@ -1,13 +1,60 @@
 package com.aile.cloud;
 
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+import com.avos.avoscloud.AVException;
+import com.avos.avoscloud.AVObject;
+import com.avos.avoscloud.SaveCallback;
+
+public class MainActivity extends Activity implements View.OnClickListener {
+
+    private Button mRequestButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initView();
+        initListener();
+    }
+
+    private void initView() {
+        mRequestButton = (Button) findViewById(R.id.main_request);
+    }
+
+    private void initListener() {
+        if (mRequestButton != null) {
+            mRequestButton.setOnClickListener(this);
+        }
+    }
+
+    @Override
+    public void onClick(View view) {
+
+        switch (view.getId()) {
+            case R.id.main_request:
+                testRequest();
+                break;
+        }
+
+    }
+
+    private void testRequest() {
+        // 测试 SDK 是否正常工作的代码
+        AVObject testObject = new AVObject("TestObject");
+        testObject.put("words", "request test!");
+        testObject.saveInBackground(new SaveCallback() {
+            @Override
+            public void done(AVException e) {
+                if (e == null) {
+                    Log.d("saved", "success!");
+                }
+            }
+        });
     }
 }
